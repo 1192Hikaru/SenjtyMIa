ce924aa4a41df425c38782aa0dfc0549fa572b9c
1192Hikaru <152240815+1192Hikaru@users.noreply.github.com>
2024-10-17 17:29:07 +0900

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var map = L.map('map').setView([35.6895, 139.6917], 10); // Tokyoの緯度経度
 L.tileLayer('MPcreate1017/{z}/{x}/{y}.png', { // タイルのパスを指定
     maxZoom: 20, // 使用しているタイルのズームレベルに合わせる
     tileSize: 256,
-    zoomOffset: -1
+    zoomOffset: 8
 }).addTo(map);
 
 // 危険な場所のマーカーを追加
