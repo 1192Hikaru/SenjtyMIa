72c28528bb9169db24ae58c59b0e4c7fca55aa11
1192Hikaru <152240815+1192Hikaru@users.noreply.github.com>
2024-11-25 16:54:34 +0900

    Update app.js

--- app.js
+++ app.js
@@ -42,6 +42,7 @@ var AdministratorIcon = L.icon({
     shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
     shadowSize: [41, 41]// 影のサイズを指定
 });
+
 var fortificationsIcon = L.icon({
     iconUrl: 'Icon/fortificationsIcon.png',
     iconSize: [41, 41],// アイコンのサイズを指定
@@ -52,7 +53,6 @@ var fortificationsIcon = L.icon({
 });
 
 // 危険な場所のマーカーを追加
-// 交通事故の起きている場所
 var dangerZones = [
     {lat: 35.9602, lng: 139.7891, name: "地点CCCACC"},
     {lat: 35.7957822, lng: 139.7728736, name: "121"},
@@ -61,17 +61,17 @@ var dangerZones = [
     {lat: 35.7720481, lng: 139.7859405, name: "交通事故No.2"},
     {lat: 35.7865950, lng: 139.7915313, name: "交通事故No.3"}
 ];
-// 不審者のいる場所の情報
+
 var suspiciousMarkers = [
     {lat: 35.6895, lng: 139.6917, name: "不審者情報1"},
     {lat: 35.6995, lng: 139.7017, name: "不審者情報2"}
 ];
-// 危険性のある交通インフラ場所の情報
+
 var agingMarkers = [
     {lat: 35.6795, lng: 139.6817, name: "老朽化情報1"},
     {lat: 35.6695, lng: 139.6717, name: "老朽化情報2"}
 ];
-// 工事の情報
+
 var constructionMarkers = [
     {lat: 35.6595, lng: 139.6617, name: "工事情報1"},
     {lat: 35.6495, lng: 139.6517, name: "工事情報2"}
@@ -112,6 +112,9 @@ var currentLocationMarker;
 // 初回位置取得済みフラグを追加
 var initialLocationSet = false;
 
+// アラートの最終表示時間を保持するオブジェクトを作成
+var lastAlertTime = {};
+
 // 音楽を再生する関数
 var alertSound = document.getElementById('alertSound');
 
@@ -123,9 +126,13 @@ function playSound() {
 function checkDangerZones(userLatLng) {
     dangerZones.forEach(function(zone) {
         var distance = map.distance(userLatLng, [zone.lat, zone.lng]);
-        if (distance < 100) { // 100メートル以内に近づいたら警告
-            playSound(); // 音楽を鳴らす
-            alert("警告: " + zone.name + " に近づいています！");
+        if (distance < 10) { // 10メートル以内に近づいたら警告
+            var currentTime = new Date().getTime();
+            if (!lastAlertTime[zone.name] || (currentTime - lastAlertTime[zone.name]) > 3600000) { // 最終アラートから1時間経過しているかを確認
+                lastAlertTime[zone.name] = currentTime; // 最終アラート時間を更新
+                playSound(); // 音楽を鳴らす
+                alert("警告: " + zone.name + " に近づいています！");
+            }
         }
     });
 }
@@ -162,4 +169,5 @@ map.on('locationerror', onLocationError);
 
 // 初回の位置情報を取得
 map.locate({ setView: false, watch: true, maxZoom: 16 });
+
 
