71deb6bec22d2ef6cda4ca6268f83e2157c318fa
1192Hikaru <152240815+1192Hikaru@users.noreply.github.com>
2024-11-26 01:25:04 +0900

    Update app.js

--- app.js
+++ app.js
@@ -332,7 +332,15 @@ suspiciousMarkers.forEach(function(zone) {
     marker.bindPopup(`<b>${zone.name}</b><br>${zone.info}`);
 });
 agingMarkers.forEach(function(zone) {
-    var marker = L.marker([zone.lat, zone.lng], { icon: blueIcon }).addTo(agingLayer);
+    var icon;
+    if (zone.score === "Ⅰ") {
+        icon = Azure2;
+    } else if (zone.score === "Ⅱ") {
+        icon = ChartreuseIcon2;
+    } else if (zone.score === "Ⅲ") {
+        icon = redIcon2;
+    }
+    var marker = L.marker([zone.lat, zone.lng], { icon: Icon }).addTo(agingLayer);
     marker.bindPopup(`<b>${zone.name}</b><br>路線名: ${zone.routeName}<br>点検年: ${zone.inspectionYear}<br>判定区分: ${zone.score}<br>措置状況: ${zone.treatmentStatus}`);
 });
 constructionMarkers.forEach(function(zone) {
