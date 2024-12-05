6b6b8a3e1e8d4a700e7ee39fb7f4de309aac58ad
1192Hikaru <152240815+1192Hikaru@users.noreply.github.com>
2024-12-06 00:25:46 +0900

    Update app.js
    10ｍ以内に変更

--- app.js
+++ app.js
@@ -436,7 +436,7 @@ function checkDangerZones(userLatLng) {
             var distance = map.distance(userLatLng, [zone.lat, zone.lng]);
             var currentTime = new Date().getTime();
 
-            if (distance < 100 && !alertTriggered && (currentTime - lastAlertTimestamp) > 3000) { // 3秒間隔でアラートを一度だけ鳴らす
+            if (distance < 10 && !alertTriggered && (currentTime - lastAlertTimestamp) > 3000) { // 距離が10m以内かつ　3秒間隔でアラートを一度だけ鳴らす
                 if (!lastAlertTime[zone] || (currentTime - lastAlertTime[zone]) > 300000) { // 最終アラートから5分経過しているかを確認
                     lastAlertTime[zone] = currentTime; // 最終アラート時間を更新
                     playSound(); // 音楽を鳴らす
