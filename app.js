42c176472a777b42d9f2655bbec19ac24bf9f015
1192Hikaru <152240815+1192Hikaru@users.noreply.github.com>
2025-01-13 11:31:25 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // 地図の初期化
-var map = L.map('map').setView([35.6895, 139.6917], 13);
+var map = L.map('map').setView([35.747936, 139.806729], 13);
 
 // タイルレイヤーの追加
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
